f32f9d7a866ef7491a87bbb991120f060ed7a1b0
Mahdis Amin <mahdis.amin95@gmail.com>
2024-08-18 22:10:09 +0330

    bug about alert and empy inputs fixed

--- app.js
+++ app.js
@@ -1,82 +1,49 @@
 const SubmitBtn = document.querySelector("button");
 const showAlertSuccess = document.querySelector(".alert-msg");
+// const errorInpusMsg = document.querySelectorAll(".error");
 const email = document.getElementById("email");
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 const massageInput = document.getElementById("msg-input");
-const radioInputs = document.querySelectorAll("input[name='query-type']");
-const checkbox = document.getElementById("consent-checkbox");
 const inputsArray = [firstName, lastName, email, massageInput];
-
-const isEmailValid = (email) => {
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailPattern.test(email);
-};
-
 const checkInputs = () => {
   const firstNameValue = firstName.value.trim();
   const lastNameValue = lastName.value.trim();
   const massageInputValue = massageInput.value.trim();
-  const emailValue = email.value.trim();
 
-  let inputsFilled = true;
-
-  if (!firstNameValue) {
+  let isInputFilled = true;
+  if (firstNameValue === "") {
     errorHandler(firstName);
-    inputsFilled = false;
+    isInputFilled = false;
   } else {
     successMessage(firstName);
   }
-
   if (lastNameValue === "") {
     errorHandler(lastName);
-    inputsFilled = false;
+    isInputFilled = false;
   } else {
     successMessage(lastName);
   }
-
   if (massageInputValue === "") {
     errorHandler(massageInput);
-    inputsFilled = false;
+    isInputFilled = false;
   } else {
     successMessage(massageInput);
   }
-
-  if (emailValue === "" || !isEmailValid(emailValue)) {
-    errorHandler(email);
-    inputsFilled = false;
-  } else {
-    successMessage(email);
-  }
-
-  let radioChecked = false;
-  radioInputs.forEach((radio) => {
-    if (radio.checked) {
-      radioChecked = true;
-    }
-  });
-
-  if (!radioChecked) {
-    errorHandler(radioInputs[0].parentElement);
-    inputsFilled = false;
-  } else {
-    successMessage(radioInputs[0].parentElement);
-  }
-
-  return inputsFilled;
+  return isInputFilled;
 };
 
 const errorHandler = (input) => {
   input.style.borderColor = "#d73c3c";
   const inputContainer = input.parentElement;
-  const errorSpan = inputContainer.querySelector(".error");
+  const errorSpan = inputContainer.querySelector("span");
   errorSpan.style.display = "block";
 };
 
 const successMessage = (input) => {
-  input.style.borderColor = "#0c7d69";
+  input.style.borderColor = "#2b4246";
   const inputContainer = input.parentElement;
-  const errorSpan = inputContainer.querySelector(".error");
+  const errorSpan = inputContainer.querySelector("span");
   errorSpan.style.display = "none";
 };
 
@@ -86,17 +53,14 @@ const showAlertHandler = () => {
     showAlertSuccess.style.display = "none";
   }, 4000);
 };
-
-const showSuccessMsg = (event) => {
-  event.preventDefault(); // جلوگیری از ارسال فرم
+const showSuccessMsg = () => {
   if (checkInputs()) {
     showAlertHandler();
+    inputsArray.forEach((input) => {
+      input.value = "";
+      input.style.borderColor = "";
+    });
   }
 
-  inputsArray.forEach((input) => {
-    input.value = "";
-    input.style.borderColor = "";
-  });
 };
-
 SubmitBtn.addEventListener("click", showSuccessMsg);
